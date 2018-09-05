815354b8c24480b15886ceb8f5ffc3f5e2add750
ymk <yamamotoakira1125@gmail.com>
2018-09-06 00:25:34 +0900

    Update styles

--- frontpage/src/pages/index.js
+++ frontpage/src/pages/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import {
+  Wrapper,
   LayoutHeader,
   Section,
+  SectionContainer,
   ContentsGrid,
   Footer,
   TimeLineWrapper,
@@ -17,63 +19,67 @@ import {TypeBodyLevel1Center, TypeBoldItalic, Copyright} from '../components/sty
 import {Spacer} from '../components/styledUtils'
 
 const IndexPage = () => (
-  <div>
+  <Wrapper>
     <LayoutHeader>
       <GlobalHeader />
     </LayoutHeader>
     <MainVisual />
-    <Section>
-      <Spacer size={72} />
-      <SectionHeader header='Why Riker'/>
-      <Spacer size={40} />
-      <TypeBodyLevel1Center>We believe there is no greater need than now for a full-featured actor model implementation that scales to hundreds or thousands of microservices and that equally can run exceptionally well on resource limited hardware to drive drones, IoT and robotics. The Rust language makes this possible.</TypeBodyLevel1Center>
-      <Spacer size={72} />
-      <ContentsGrid>
-        <Content
-          header='Actor Based Concurrency'
-          body='Actors are lightweight abstractions that make it easier to develop concurrent software. Actors expose an API via messaging resulting in fast, non-blocking code execution while also eliminating race conditions to make concurrent code a breeze to write.'
-        />
-        <Content
-          header='Resilient & Self Healing'
-          body='Build applications that isolate and recover from failures. Actors provide a Supervision Strategy that form the core of resilient application design in Riker.'
-        />
-        <Content
-          header='Event Sourcing & CQRS'
-          body='Use event sourcing and Command Query Responsibility Separation (CQRS) to drive extremely fast persistent data applications.'
-        />
-        <Content
-          header='Modern'
-          body='Scale Microservices to hundreds of instances, or run high performance drone systems on limited hardware. Riker applications compile to OS binaries (thanks to Rust), have no VM overhead, and require only a few megabytes of memory to run.'
-        />
-      </ContentsGrid>
+    <Section bg={'#F1F3F5'}>
+      <SectionContainer>
+        <Spacer size={56} />
+        <SectionHeader header='Why Riker'/>
+        <Spacer size={40} />
+        <TypeBodyLevel1Center>We believe there is no greater need than now for a full-featured actor model implementation that scales to hundreds or thousands of microservices and that equally can run exceptionally well on resource limited hardware to drive drones, IoT and robotics. The Rust language makes this possible.</TypeBodyLevel1Center>
+        <Spacer size={72} />
+        <ContentsGrid>
+          <Content
+            header='Actor Based Concurrency'
+            body='Actors are lightweight abstractions that make it easier to develop concurrent software. Actors expose an API via messaging resulting in fast, non-blocking code execution while also eliminating race conditions to make concurrent code a breeze to write.'
+          />
+          <Content
+            header='Resilient & Self Healing'
+            body='Build applications that isolate and recover from failures. Actors provide a Supervision Strategy that form the core of resilient application design in Riker.'
+          />
+          <Content
+            header='Event Sourcing & CQRS'
+            body='Use event sourcing and Command Query Responsibility Separation (CQRS) to drive extremely fast persistent data applications.'
+          />
+          <Content
+            header='Modern'
+            body='Scale Microservices to hundreds of instances, or run high performance drone systems on limited hardware. Riker applications compile to OS binaries (thanks to Rust), have no VM overhead, and require only a few megabytes of memory to run.'
+          />
+        </ContentsGrid>
+      </SectionContainer>
+      <Spacer size={120} />
     </Section>
     <Section >
-      <Spacer size={72} />
-      <SectionHeader header='Roadmap'/>
-      <Spacer size={72} />
-      <TimeLineWrapper>
-        <TimeLineBullet>
-          <TimeLineBulletList>
-            <TypeBoldItalic>Remote actors</TypeBoldItalic>
-          </TimeLineBulletList>
-          <TimeLineBulletList>
-            <TypeBoldItalic>Support for TCP and UDP</TypeBoldItalic>
-          </TimeLineBulletList>
-          <TimeLineBulletList>
-            <TypeBoldItalic>Support for TCP and UDP</TypeBoldItalic>
-          </TimeLineBulletList>
-          <TimeLineBulletList>
-            <TypeBoldItalic>Distributed data (CRDTs)</TypeBoldItalic>
-          </TimeLineBulletList>
-        </TimeLineBullet>
-      </TimeLineWrapper>
+      <SectionContainer>
+        <Spacer size={72} />
+        <SectionHeader header='Roadmap'/>
+        <Spacer size={72} />
+        <TimeLineWrapper>
+          <TimeLineBullet>
+            <TimeLineBulletList>
+              <TypeBoldItalic>Remote actors</TypeBoldItalic>
+            </TimeLineBulletList>
+            <TimeLineBulletList>
+              <TypeBoldItalic>Support for TCP and UDP</TypeBoldItalic>
+            </TimeLineBulletList>
+            <TimeLineBulletList>
+              <TypeBoldItalic>Support for TCP and UDP</TypeBoldItalic>
+            </TimeLineBulletList>
+            <TimeLineBulletList>
+              <TypeBoldItalic>Distributed data (CRDTs)</TypeBoldItalic>
+            </TimeLineBulletList>
+          </TimeLineBullet>
+        </TimeLineWrapper>
+      </SectionContainer>
+      <Spacer size={160} />
     </Section>
-    <Spacer size={72} />
     <Footer>
       <Copyright>&copy;Riker</Copyright>
     </Footer>
-    <Spacer size={72} />
-  </div>
+  </Wrapper>
 )
 
 export default IndexPage
--- frontpage/src/pages/styled.js
+++ frontpage/src/pages/styled.js
@@ -1,5 +1,9 @@
 import styled from 'styled-components'
 
+export const Wrapper = styled.div`
+  overflow: hidden;
+`
+
 export const LayoutHeader = styled.div`
   position: absolute;
   top: 0;
@@ -12,8 +16,25 @@ export const LayoutHeader = styled.div`
 `
 
 export const Section = styled.section`
+  position: relative;
+  &::after {
+    background: ${props => props.bg || '#fff'};
+    position: absolute;
+    display: block;
+    content: ' ';
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+    transform-origin: left;
+    transform: skewY(-7deg);
+  }
+`
+
+export const SectionContainer = styled.div`
   margin: auto;
-  max-width: 1200px;
+  max-width: 1000px;
   width: 96%;
 `
 
@@ -23,9 +44,29 @@ export const ContentsGrid = styled.div`
   flex-wrap: wrap;
 `
 
-export const Footer = styled.p`
+export const Footer = styled.div`
+  color: #fff;
   font-size: 12px;
   font-style: italic;
+  position: relative;
+  p {
+    position: absolute;
+    right: 4%;
+    bottom: 16px;
+  }
+  &::after {
+    background-image: linear-gradient(150deg, #2F0070 0%, #10D2FB 100%);
+    position: absolute;
+    display: block;
+    content: ' ';
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 400px;
+    z-index: -1;
+    transform-origin: left;
+    transform: skewY(-7deg);
+  }
 `
 
 export const TimeLineWrapper = styled.div`
