38b7ccde9658a09aa9929efb024922f650767a20
ymk <yamamotoakira1125@gmail.com>
2018-10-03 21:46:22 +0900

    Add text-shadow in button

--- frontpage/src/components/Atoms/Button/styled.js
+++ frontpage/src/components/Atoms/Button/styled.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 export const Button = styled.button`
   border-radius: 8px;
   box-shadow: 0 2px 2px rgba(0, 0, 0, 0.1);
+  text-shadow: 0 1px 3px rgba(0,0,0,.2);
   color: #fff;
   font-weight: bold;
   background: #0EDC69;
