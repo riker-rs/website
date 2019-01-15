d835288f107e4b5d297366107388d42d5a00b90d
ymk <yamamotoakira1125@gmail.com>
2019-01-15 21:06:51 +0900

    Migration for GatsbyV2

--- frontpage/src/pages/index.js
+++ frontpage/src/pages/index.js
@@ -9,6 +9,7 @@ import {
   Pre,
   LineNo
 } from '../styled'
+import Layout from '../components/Layout'
 import GlobalHeader from '../components/Molecules/GlobalHeader'
 import MainVisual from '../components/Organisms/MainVisual'
 import Content from '../components/Molecules/Content'
@@ -41,67 +42,69 @@ a.tell("Hello actor!".to_string(), None);
 `;
 
 const IndexPage = () => (
-  <Wrapper>
-    <LayoutHeader>
-      <GlobalHeader />
-    </LayoutHeader>
-    <MainVisual />
-    <Section bg={'#F1F3F5'}>
-      <SectionContainer>
-        <Spacer size={30} />
-        <ContentsGrid>
-          <Content
-            header='Actor Based Concurrency'
-            body='Actors expose a message based API resulting in fast, non-blocking code execution while also eliminating race conditions to make concurrent code a breeze to write.'
-          />
-          <Content
-            header='Self Healing'
-            body='Build applications that isolate and recover from failures. Actors provide a Supervision Strategy that form the core of resilient application design in Riker.'
-          />
-          <Content
-            header='Event Sourcing & CQRS'
-            body='Use event sourcing and Command Query Responsibility Separation (CQRS) to drive extremely fast persistent data applications.'
-          />
-          <Content
-            header='General Purpose'
-            body='In the Cloud, Microservices, IoT, Drones, AI and more. Rust compiles to OS binaries, has no VM or GC overhead, and requires only a few megabytes of memory.'
-          />
-          <Content
-            header='Modular Framework'
-            body='Default thread management, concurrent logging, message scheduling and data persistence are included and can be switched for alternative implementations.'
-          />
-          <Content
-            header='Modern Rust Design'
-            body='Execute Rust Futures with ease and no unsafe code used.'
-          />
-        </ContentsGrid>
-      </SectionContainer>
-      <Spacer size={80} />
-    </Section>
-    <Section >
-      <SectionContainer>
-        <Spacer size={20} />
-        <SectionHeader header='Example Code'/>
-        <Spacer size={24} />
-        <Highlight {...defaultProps} theme={theme} code={exampleCode} language="jsx">
-          {({ className, style, tokens, getLineProps, getTokenProps }) => (
-            <Pre className={className} style={style}>
-              {tokens.map((line, i) => (
-                <div {...getLineProps({ line, key: i })}>
-                  <LineNo>{i + 1}</LineNo>
-                  {line.map((token, key) => <span {...getTokenProps({ token, key })} />)}
-                </div>
-              ))}
-            </Pre>
-          )}
-        </Highlight>
-      </SectionContainer>
-      <Spacer size={160} />
-    </Section>
-    <Footer>
-      <Copyright>&copy; Riker 2019</Copyright>
-    </Footer>
-  </Wrapper>
+  <Layout>
+    <Wrapper>
+      <LayoutHeader>
+        <GlobalHeader />
+      </LayoutHeader>
+      <MainVisual />
+      <Section bg={'#F1F3F5'}>
+        <SectionContainer>
+          <Spacer size={30} />
+          <ContentsGrid>
+            <Content
+              header='Actor Based Concurrency'
+              body='Actors expose a message based API resulting in fast, non-blocking code execution while also eliminating race conditions to make concurrent code a breeze to write.'
+            />
+            <Content
+              header='Self Healing'
+              body='Build applications that isolate and recover from failures. Actors provide a Supervision Strategy that form the core of resilient application design in Riker.'
+            />
+            <Content
+              header='Event Sourcing & CQRS'
+              body='Use event sourcing and Command Query Responsibility Separation (CQRS) to drive extremely fast persistent data applications.'
+            />
+            <Content
+              header='General Purpose'
+              body='In the Cloud, Microservices, IoT, Drones, AI and more. Rust compiles to OS binaries, has no VM or GC overhead, and requires only a few megabytes of memory.'
+            />
+            <Content
+              header='Modular Framework'
+              body='Default thread management, concurrent logging, message scheduling and data persistence are included and can be switched for alternative implementations.'
+            />
+            <Content
+              header='Modern Rust Design'
+              body='Execute Rust Futures with ease and no unsafe code used.'
+            />
+          </ContentsGrid>
+        </SectionContainer>
+        <Spacer size={80} />
+      </Section>
+      <Section >
+        <SectionContainer>
+          <Spacer size={20} />
+          <SectionHeader header='Example Code'/>
+          <Spacer size={24} />
+          <Highlight {...defaultProps} theme={theme} code={exampleCode} language="jsx">
+            {({ className, style, tokens, getLineProps, getTokenProps }) => (
+              <Pre className={className} style={style}>
+                {tokens.map((line, i) => (
+                  <div {...getLineProps({ line, key: i })}>
+                    <LineNo>{i + 1}</LineNo>
+                    {line.map((token, key) => <span {...getTokenProps({ token, key })} />)}
+                  </div>
+                ))}
+              </Pre>
+            )}
+          </Highlight>
+        </SectionContainer>
+        <Spacer size={160} />
+      </Section>
+      <Footer>
+        <Copyright>&copy; Riker 2019</Copyright>
+      </Footer>
+    </Wrapper>
+  </Layout>
 )
 
 export default IndexPage
