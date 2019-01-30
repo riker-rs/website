2a1684deff2d5f641201690b199c40bb296a6239
ymk <yamamotoakira1125@gmail.com>
2019-01-30 21:25:39 +0900

    Fix typo

--- frontpage/src/pages/index.js
+++ frontpage/src/pages/index.js
@@ -18,12 +18,12 @@ import {Copyright} from '../components/styledTypography'
 import {Spacer} from '../components/styledUtils'
 import Highlight, { defaultProps } from 'prism-react-renderer'
 import theme from 'prism-react-renderer/themes/nightOwl'
-import IconConccurent from '../Components/Atoms/Icons/icon-cuncurrent.png'
-import IconEventSoursing from '../Components/Atoms/Icons/icon-event-sourcing.png'
-import IconGeneral from '../Components/Atoms/Icons/icon-general.png'
-import IconModern from '../Components/Atoms/Icons/icon-modern.png'
-import IconModular from '../Components/Atoms/Icons/icon-modular.png'
-import IconSelfHeal from '../Components/Atoms/Icons/icon-self-heal.png'
+import IconConccurent from '../components/Atoms/Icons/icon-cuncurrent.png'
+import IconEventSoursing from '../components/Atoms/Icons/icon-event-sourcing.png'
+import IconGeneral from '../components/Atoms/Icons/icon-general.png'
+import IconModern from '../components/Atoms/Icons/icon-modern.png'
+import IconModular from '../components/Atoms/Icons/icon-modular.png'
+import IconSelfHeal from '../components/Atoms/Icons/icon-self-heal.png'
 
 const exampleCode = `struct MyActor;
 
